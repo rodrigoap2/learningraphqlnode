f1b03aa9aba10fd45e2274c869c0d3f9951e5f2f
rodrigoap2 <rap2@cin.ufpe.br>
2021-01-16 23:53:53 -0300

    Create CRUD with mutations

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 const { ApolloServer } = require('apollo-server');
-
-const typeDefs = `
-    type Query {
-        info : String!
-        feed: [Link!]!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`
+const fs = require('fs');
+const path = require('path');
 
 let links = [{
     id: 'link-0',
@@ -19,10 +8,47 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
   }]
 
+let idCount = links.length
+
 const resolvers = {
     Query: {
         info: () => `This is the API of Hackernews Clone`,
-        feed: () => links
+        feed: () => links,
+        link: (parent, args) => links.find((link) => link.id == args.id)
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        },
+        //updateLink(id: ID!, url: String, description: String): Link
+        updateLink: (parent, args) => {
+            const link = {
+                id: args.id,
+                url: args.url,
+                description: args.description
+            }
+            const position = links.findIndex((link) => link.id == args.id)
+            if(position == -1){
+                return null;
+            }else {
+                links[position] = link
+                return links[position];
+            }
+        }, 
+        //deleteLink(id: ID!): Link
+        deleteLink: (parent, args) => {
+            const position = links.findIndex((link) => link.id == args.id)
+            if(position > -1){
+                links.splice(position, 1)
+            }
+            return null
+        }
     },
     Link: {
         id: (parent) => parent.id,
@@ -32,7 +58,10 @@ const resolvers = {
 }
 
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
     resolvers
 })
 
